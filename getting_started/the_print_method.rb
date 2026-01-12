bc3d845f56bf9e3114c93466770d81b4dfb04b1c
veezdev <luthfizonedev@gmail.com>
2026-01-12 20:49:08 +0700

    the print method

---
+++ getting_started/the_print_method.rb
@@ -0,0 +1,11 @@
+# The print method in Ruby is used to print output to the console.
+# Unlike puts, it does not add a new line after each argument passed to it.
+# It will also return nil.
+
+print "Hello, World! "
+print 3 + 5
+
+# Parentheses are optional in ruby
+# and we can combine with single quotes in double quotes like below example
+print("Ruby is fun! ", "Let's learn Ruby together. ")
+
