9173e4a954ba1f9798a086659533d928c9fc1a28
veezdev <luthfizonedev@gmail.com>
2026-01-12 20:40:45 +0700

    the puts method

---
+++ getting_started/the_puts_method.rb
@@ -0,0 +1,17 @@
+# The puts method in Ruby is used to print output to the console.
+# It adds a new line after each argument passed to it.
+# It will return nil
+
+puts "Hello, World!"
+
+puts 3 + 5
+
+# Parentheses are optional in ruby
+# and we can combile with single quotes in double quotes like below example
+puts("Ruby is fun!", "Let's learn Ruby together.")
+
+# Multiline using comma (,)
+puts 5
+puts 3
+puts 2
+puts 5, 3, 2
