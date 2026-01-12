43de3ced608d16ccec85b22c6fac748ffeb24737
veezdev <luthfizonedev@gmail.com>
2026-01-12 20:51:00 +0700

    the p method

---
+++ getting_started/the_p_method.rb
@@ -0,0 +1,16 @@
+# The p method in Ruby is used to print output to the console.
+# It calls the inspect method on the object, which provides a more detailed representation.
+# This is particularly useful for debugging, as it shows special characters and data structures clearly.
+# It will return the object passed to it.
+
+p "Hello, World!"
+
+p 3 + 5
+
+# Parentheses are optional in ruby
+# and we can combine with single quotes in double quotes like below example
+p("Ruby is fun!", "Let's learn Ruby together.")
+
+
+# if we write a string with the value of number it will return string in inspect format
+p "123"
