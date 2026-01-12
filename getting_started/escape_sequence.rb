906d33cb49cc8eca7f2b5dc9c56dfd2110cc86fd
veezdev <luthfizonedev@gmail.com>
2026-01-12 20:47:28 +0700

    escape sequence

---
+++ getting_started/escape_sequence.rb
@@ -0,0 +1,22 @@
+# Escape sequences are special characters in strings that are used to represent certain whitespace or control characters.
+# They are preceded by a backslash (\) and have specific meanings.
+
+# Common escape sequences in Ruby include:
+# \n - New line
+# \t - Tab
+# \\ - Backslash
+# \' - Single quote
+# \" - Double quote
+
+puts "Hello,\nSkha"
+
+puts "Item 1\tItem 2\tItem 3"
+
+puts "This is a backslash: \\"
+
+puts 'It\'s a beautiful day!'
+
+puts "He said, \"Ruby is awesome!\""
+
+# You can combine multiple escape sequences in a single string
+puts "Line 1\nLine 2\t\twith a tab\nLine 3 with a backslash: \\"
