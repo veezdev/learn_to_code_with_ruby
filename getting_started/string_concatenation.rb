7dcef750e9a3e1290a417b9417f402b0795eb423
veezdev <luthfizonedev@gmail.com>
2026-01-13 08:16:56 +0700

    string concatenation

---
+++ getting_started/string_concatenation.rb
@@ -0,0 +1,9 @@
+# String concatenation in Ruby can be done in several ways. Here are some common methods:
+# Using the + operator
+puts "Hello, " + "world!"  # Output: Hello, world!
+
+# Using the << operator (shovel operator)
+puts "Hello, " << "world!"  # Output: Hello, world!
+
+# Using string methods (e.g., concat method)
+puts "Hello, ".concat("world!")  # Output: Hello, world!
