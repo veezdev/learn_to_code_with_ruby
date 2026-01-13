e6ab84ee893ba69080334bf10ec38de552b50a4a
veezdev <luthfizonedev@gmail.com>
2026-01-13 08:09:15 +0700

    integer and floating point number

---
+++ getting_started/integer_and_floating_number.rb
@@ -0,0 +1,12 @@
+# Integer numbers are whole numbers without a decimal point.
+
+puts 100 # positive integer
+puts(-42) # negative integer (best practice to use parentheses)
+puts 0 # zero
+puts 1_000 # underscores can be used for better readability (same like PHP's numeric literals)
+
+# Floating-point numbers (or floats) are numbers with a decimal point.
+puts 3.14 # positive float
+puts(-0.001) # negative float
+puts(-10.001) # negative float with trailing zeros
+puts 0.0 # float representation of zero
