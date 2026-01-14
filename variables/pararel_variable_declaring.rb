464bbeee7d81e7b0a12f5e684940b5f57b1b9c44
veezdev <luthfizonedev@gmail.com>
2026-01-14 14:49:20 +0700

    pararel variable

---
+++ variables/pararel_variable_declaring.rb
@@ -0,0 +1,17 @@
+a = 10
+b = 20
+c = 30
+puts a
+puts b
+puts c
+
+# We can also declare a multiple variables in a single line
+x, y, z = 100, 200, 300
+puts x
+puts y
+puts z
+
+=begin
+ For more common developer use not using a parallel variable declaring like above,
+ because it can reduce the code readability. 
+=end
