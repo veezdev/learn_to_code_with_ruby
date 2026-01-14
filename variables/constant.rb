88a0e7a7c9fd8fd1b5aea0a16c598f3d9e86393e
veezdev <luthfizonedev@gmail.com>
2026-01-14 15:00:14 +0700

    constant

---
+++ variables/constant.rb
@@ -0,0 +1,8 @@
+PI = Math::PI
+puts PI
+
+# ruby will not return error if we try to change the value of constant
+# but it will give a warning message
+# best practice is to not change the value of constant variable value
+PI = 3.14
+puts PI
