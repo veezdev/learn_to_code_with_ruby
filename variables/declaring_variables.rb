94c7ee228fbb17178cc7cf78ff4b20bfc7a07730
veezdev <luthfizonedev@gmail.com>
2026-01-21 06:55:37 +0700

    declaring variables

---
+++ variables/declaring_variables.rb
@@ -0,0 +1,13 @@
+# Declaring a variable in Ruby
+# Same with other's languages, we can declare a variable by simply assigning a value to it.
+# The ruby language is similar to python in this aspect.
+
+age = 25
+puts age
+
+first_name = "Muhammad"
+last_name = "Luthfi"
+puts first_name + " " + last_name
+
+age_next_year = age + 1
+puts "Next year, I will be #{age_next_year} years old." # Using string interpolation
