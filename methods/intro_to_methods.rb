bafc8715ef007bd1f3d7a973e56f32b947595fe2
veezdev <luthfizonedev@gmail.com>
2026-01-21 07:48:09 +0700

    intro to method

---
+++ methods/intro_to_methods.rb
@@ -0,0 +1,17 @@
+# Methods it same with functions in other programming languages
+# A method is a set of code that is grouped together to perform a specific task.
+# Methods help to organize code into reusable blocks, making it easier to read,
+# maintain, and debug.
+
+# own custom method
+def say_hello
+  puts "Hello, World!"
+  puts "Welcome to learning Ruby methods."
+  puts "Enjoy coding!"
+end
+
+# calling the method without parentheses
+say_hello
+
+# calling the method with parentheses
+say_hello()
