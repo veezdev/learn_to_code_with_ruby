bed777e971e6c0db9c89b93be56418773e943bd9
veezdev <luthfizonedev@gmail.com>
2026-01-21 16:11:28 +0700

    falsy and truthy

---
+++ conditional/falsy_and_truthy.rb
@@ -0,0 +1,6 @@
+# In ruby everything is truthy except for false and nil (even the empty string is a truthy).
+# This is more simple than JS or other languages
+
+if "".nil?
+  puts "Empty string is a nil"
+end
