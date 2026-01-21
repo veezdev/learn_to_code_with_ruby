50952ee551f44404c5e9004d9b42617bed9e64e5
veezdev <luthfizonedev@gmail.com>
2026-01-21 07:06:09 +0700

    assignment operator

---
+++ variables/assignment_operator.rb
@@ -0,0 +1,19 @@
+# The assignment operator in Ruby same like other programming languages is the equal sign (=).
+# It is used to assign a value to a variable.
+# we can use for number or string values.
+# for number data types we can use all math operator like +, -, *, /, %, **
+
+# Assigning a number to a variable
+number = 10
+puts number  # Output: 10
+
+# Assigning a string to a variable
+greeting = "Hello, "
+puts greeting  # Output: Hello, 
+
+# We can also reassign a new value to the same variable
+number += 20
+puts number  # Output: 30
+
+greeting += "Veez"
+puts greeting  # Output: Hello, Veez
