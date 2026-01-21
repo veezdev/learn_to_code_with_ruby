83ae58afafb3072d338f00dabf9863fd77cc7139
veezdev <luthfizonedev@gmail.com>
2026-01-21 21:33:56 +0700

    multiline string

---
+++ string_1/multiline_string.rb
@@ -0,0 +1,8 @@
+text = <<MLS
+  MLS  Halo semua ini adalah contoh dari multiline string
+  atau heredoc di Ruby, semisal kita menggunakan escape sequence
+  akan langsung berdampak pada kode kita juga termasuk enter dan apapun itu
+    - Veez
+MLS
+
+puts text
