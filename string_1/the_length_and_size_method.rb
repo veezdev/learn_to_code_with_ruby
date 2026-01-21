62e39f4ae840efb39721290ba9af8c11ec5df5cf
veezdev <luthfizonedev@gmail.com>
2026-01-21 21:38:08 +0700

    length and size method

---
+++ string_1/the_length_and_size_method.rb
@@ -0,0 +1,9 @@
+# The lenght and size method are used to determine the number of characters in a string.
+# Both methods return the same result and can be used interchangeably.
+
+name = "Veez"
+puts name.length  # Output: 4
+puts name.size    # Output: 4
+
+puts "   .halo2$".size  # Output: 7 (includes spaces and special characters)
+puts "".length       # Output: 0 (empty string has length 0)
