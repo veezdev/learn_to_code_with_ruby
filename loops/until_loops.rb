3150de0bcc0449a9d94c6e920fa0ae71c92849cf
veezdev <luthfizonedev@gmail.com>
2026-01-23 19:29:20 +0700

    until loops

---
+++ loops/until_loops.rb
@@ -0,0 +1,11 @@
+# It sames like unless and if else statement
+# Until loop will run until the condition is met (false)
+
+count = 0
+
+until count > 10
+  puts "Count is: #{count}"
+  count += 1
+end
+
+puts count
